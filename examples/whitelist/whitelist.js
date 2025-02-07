741a8ce4b673f98bb234a296e26e577ff8f1b2d9
browser-vm <namelessonbandlab@outlook.com>
2025-02-07 13:21:56 +0000

    Updated whitelist.js to disable eslint rule

--- examples/whitelist/whitelist.js
+++ examples/whitelist/whitelist.js
@@ -1,5 +1,7 @@
 "use strict";
 
+/* eslint-disable node/no-unsupported-features/node-builtins */
+
 module.exports = function ({ allowedDomains, message }) {
   function isRequestAllowed(data) {
     let hostname;
